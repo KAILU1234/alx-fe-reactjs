5c85cb442c656771ac87e3b30384dca690a84ccf
Munkailu Yakubu <munkailu2003yakubu@gmail.com>
2025-11-21 08:32:13 +0000

    Task 0: Project setup for GitHub User Search Application

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>GitHub User Search Application</h1>
+      <p>Project setup successful. Start building your components.</p>
+    </div>
+  );
+}
+
+export default App;
