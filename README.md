9cc300f7671e03cf3306a9af9152945de6126ad1
Munkailu Yakubu <munkailu2003yakubu@gmail.com>
2025-12-17 01:12:53 +0000

    Inline CSS styling task

---
+++ README.md
@@ -0,0 +1,12 @@
+# alx-react-app-new
+
+This project demonstrates inline CSS styling in React components as part of the ALX Frontend React curriculum.
+
+## Features
+- Inline styled React components
+- Functional components
+- Basic component structure
+
+## Run Project
+npm install  
+npm run dev
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './components/Header';
+import UserProfile from './components/UserProfile';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <UserProfile name="John Doe" age={30} bio="React developer" />
+      <MainContent />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
