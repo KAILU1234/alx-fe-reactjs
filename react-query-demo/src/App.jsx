77f17b3a3ef4e1dce8c78a9d6531674df782276c
Munkailu Yakubu <munkailu2003yakubu@gmail.com>
2025-12-18 10:05:33 +0000

    Add React Query setup and PostsComponent fetching posts

---
+++ react-query-demo/src/App.jsx
@@ -0,0 +1,11 @@
+import PostsComponent from "./components/PostsComponent.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <PostsComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,29 @@
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const fetchPosts = async () => {
+  const { data } = await axios.get("https://jsonplaceholder.typicode.com/posts");
+  return data;
+};
+
+export default function PostsComponent() {
+  const { data, error, isLoading, refetch } = useQuery("posts", fetchPosts);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Something went wrong!</p>;
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <button onClick={refetch}>Refetch Posts</button>
+      <ul>
+        {data.map((post) => (
+          <li key={post.id}>
+            <strong>{post.title}</strong>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
