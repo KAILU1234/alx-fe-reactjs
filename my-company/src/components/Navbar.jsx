06dce3b357334283e966d19f2a2f329f059518cc
Munkailu Yakubu <munkailu2003yakubu@gmail.com>
2025-12-18 06:58:16 +0000

    Add company website with routing and contact form

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,12 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', backgroundColor: '#333' }}>
+    <nav style={{ 
+      display: 'flex', 
+      justifyContent: 'space-between', 
+      padding: '10px', 
+      backgroundColor: '#333' 
+    }}>
       <Link style={{ color: 'white', marginRight: '10px' }} to="/">Home</Link>
       <Link style={{ color: 'white', marginRight: '10px' }} to="/about">About</Link>
       <Link style={{ color: 'white', marginRight: '10px' }} to="/services">Services</Link>
