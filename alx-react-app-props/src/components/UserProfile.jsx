1c5eec970c295e9c1e2c90844c2117185142f83a
Munkailu Yakubu <munkailu2003yakubu@gmail.com>
2025-12-18 06:17:17 +0000

    Add UserProfile component using Context API

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,7 +1,15 @@
-import UserInfo from './UserInfo';
+import { useContext } from 'react';
+import UserContext from './UserContext';
 
 function UserProfile() {
-  return <UserInfo />;
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
 }
 
 export default UserProfile;
