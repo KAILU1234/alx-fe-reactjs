2ef1107acf6bc67e63bc5cedbc79e047bbf7e373
Munkailu Yakubu <munkailu2003yakubu@gmail.com>
2025-12-18 01:00:41 +0000

    Add Counter component using useState

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,16 +1,10 @@
-import Header from './components/Header';
-import UserProfile from './components/UserProfile';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
+import Counter from './components/Counter';
 
 function App() {
   return (
-    <>
-      <Header />
-      <UserProfile name="John Doe" age={30} bio="React developer" />
-      <MainContent />
-      <Footer />
-    </>
+    <div>
+      <Counter />
+    </div>
   );
 }
 
